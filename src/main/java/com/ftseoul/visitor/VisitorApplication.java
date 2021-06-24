c56323cc9be56739eef01960516b6543e4e6c442
Ahn YoungSeon <dudtjs0920@naver.com>
2021-06-24 16:08:21 +0900

    initial commit

---
+++ src/main/java/com/ftseoul/visitor/VisitorApplication.java
@@ -0,0 +1,13 @@
+package com.ftseoul.visitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftseoul/visitor/VisitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftseoul.visitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
