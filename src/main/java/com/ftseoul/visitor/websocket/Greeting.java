3dbe41edc2b439c4da0776ea3a7807ad3c369996
Ahn YoungSeon <dudtjs0920@naver.com>
2021-07-02 14:41:25 +0900

    [edit] lombok -> @ add

--- src/main/java/com/ftseoul/visitor/websocket/Greeting.java
+++ src/main/java/com/ftseoul/visitor/websocket/Greeting.java
@@ -1,19 +1,19 @@
 package com.ftseoul.visitor.websocket;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
 public class Greeting {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String content;
-
-    public Greeting() {
-    }
 
     public Greeting(String content) {
         this.content = content;
