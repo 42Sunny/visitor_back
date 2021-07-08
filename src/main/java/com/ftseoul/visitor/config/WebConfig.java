d0dc7e53ff905b19785efd16b404659f622e4ceb
Ahn YoungSeon <dudtjs0920@naver.com>
2021-07-08 16:45:15 +0900

    [fix] CORS config fix

--- src/main/java/com/ftseoul/visitor/config/WebConfig.java
+++ src/main/java/com/ftseoul/visitor/config/WebConfig.java
@@ -1,13 +1,18 @@
 package com.ftseoul.visitor.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedHeaders("*")
-                .allowedOrigins("*");
+                .allowedMethods("GET", "OPTIONS", "POST", "HEAD", "DELETE", "PUT", "PATCH")
+                .maxAge(3600)
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:3000");
     }
 }
