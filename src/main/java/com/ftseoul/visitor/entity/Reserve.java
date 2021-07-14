7a557efd1262428f88c3f0761848850ee5bff3ce
최재형 <dang0764@naver.com>
2021-07-14 22:53:24 +0900

    [edit] Edit entities
    - add constructors
    - add update functions

--- src/main/java/com/ftseoul/visitor/entity/Reserve.java
+++ src/main/java/com/ftseoul/visitor/entity/Reserve.java
@@ -6,11 +6,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "reserve")
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reserve {
 
     @Id
@@ -25,9 +29,37 @@ public class Reserve {
 
     private String visitorName;
 
-    private String visitor;
+    private String visitorPhone;
 
     private String purpose;
 
     private LocalDateTime date;
+
+    public void updatePlace(String place) {
+        this.place = place;
+    }
+
+    public void updateTargetStaff(Long targetStaff) {
+        this.targetStaff = targetStaff;
+    }
+
+    public void updateVisitorOrganization(String visitorOrganization) {
+        this.visitorOrganization = visitorOrganization;
+    }
+
+    public void updateVisitorName(String visitorName) {
+        this.visitorName = visitorName;
+    }
+
+    public void updateVisitorPhone(String visitorPhone) {
+        this.visitorPhone = visitorPhone;
+    }
+
+    public void updatePurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public void updateDate(LocalDateTime date) {
+        this.date = date;
+    }
 }
--- src/main/java/com/ftseoul/visitor/entity/Staff.java
+++ src/main/java/com/ftseoul/visitor/entity/Staff.java
@@ -5,12 +5,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "staff")
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Staff {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +22,12 @@ public class Staff {
     private String name;
 
     private String phone;
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void updatePhone(String phone) {
+        this.phone = phone;
+    }
 }
