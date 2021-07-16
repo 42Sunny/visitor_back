ceb4591f897a280c9bb66251cb8734c0b5f31144
최재형 <dang0764@naver.com>
2021-07-16 21:24:46 +0900

    [add] Swagger config

--- src/main/java/com/ftseoul/visitor/config/WebConfig.java
+++ src/main/java/com/ftseoul/visitor/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.ftseoul.visitor.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600)
                 .allowCredentials(true)
                 .allowedOrigins("http://localhost:3000", "https://visitor.dev.42seoul.io", "http://visitor.dev.42seoul.io");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+        registry
+            .addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
