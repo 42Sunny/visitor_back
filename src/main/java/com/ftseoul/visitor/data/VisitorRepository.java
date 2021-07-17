89bf1867de8acf7850a127309cbfdecb77236f95
최재형 <dang0764@naver.com>
2021-07-18 04:38:31 +0900

    [edit] VSTR-42 edit update
    - delete entities not in dto phone lists
    - add entities -> isChanged : true
    - delete duplicated phone number
     -> User mismatch phone and name
     -> try to change
     -> results in duplcated phone number

--- src/main/java/com/ftseoul/visitor/data/VisitorRepository.java
+++ src/main/java/com/ftseoul/visitor/data/VisitorRepository.java
@@ -1,20 +1,27 @@
 package com.ftseoul.visitor.data;
 
+import com.ftseoul.visitor.dto.VisitorModifyDto;
+import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VisitorRepository extends JpaRepository<Visitor, Long> {
     List<Visitor> findAllByNameAndPhone(String name, String phone);
     Optional<Visitor> findByName(String name);
     Optional<Visitor> findByPhone(String phone);
+    Optional<Visitor> findByReserveIdAndPhone(Long reserveId, String phone);
     List<Visitor> findAllByName(String name);
     List<Visitor> findAllByPhone(String phone);
-
     List<Visitor> findAllByReserveId(Long reserve_id);
 
     Optional<Visitor> findByNameAndPhoneAndReserveId(String name, String phone, Long reserveId);
 
     void deleteAllByReserveId(Long reserveId);
+
+    @Query("DELETE FROM Visitor v WHERE v.id = :id AND v.phone NOT IN :toDelete")
+    void updateDeletedVisitors(@Param("toDelete") Collection<String> toDelete, @Param("id") Long id);
 }
---
+++ src/main/java/com/ftseoul/visitor/dto/VisitorModifyDto.java
@@ -0,0 +1,32 @@
+package com.ftseoul.visitor.dto;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VisitorModifyDto implements Serializable {
+    private Long reserve_id;
+
+    private String name;
+
+    private String phone;
+
+    private String organization;
+
+    private boolean isChanged;
+
+    @Override
+    public String toString() {
+        return "VisitorModifyDto{" +
+            "reserve_id=" + reserve_id +
+            ", name='" + name + '\'' +
+            ", phone='" + phone + '\'' +
+            ", organization='" + organization + '\'' +
+            ", isChanged=" + isChanged +
+            '}';
+    }
+}
