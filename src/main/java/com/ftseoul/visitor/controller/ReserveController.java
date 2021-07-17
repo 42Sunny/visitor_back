9fc7ca22ae5437391281b29d4748b0ad58f4818c
Ahn YoungSeon <dudtjs0920@naver.com>
2021-07-17 15:15:39 +0900

    [edit] VSTR-41 name, phone으로 예약 조회했을 때 리턴값 수정(한 예약에 포함된 visitor list를 담아 리턴)

--- src/main/java/com/ftseoul/visitor/controller/ReserveController.java
+++ src/main/java/com/ftseoul/visitor/controller/ReserveController.java
@@ -33,8 +33,8 @@ public class ReserveController {
     }
 
     @PostMapping("/reserves")
-    public List<ReserveResponseDto> searchReserveList(@RequestBody SearchReserveRequestDto reserveRequestDto) {
-        return reserveService.findAllByNameAndPhone(reserveRequestDto);
+    public List<ReserveListResponseDto> searchReserveList(@RequestBody SearchReserveRequestDto reserveRequestDto) {
+        return reserveService.findReserveByVisitor(reserveRequestDto);
     }
 
     @DeleteMapping("/reserve")
