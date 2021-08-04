2c561f643b33a62b72783c16025f77f0b402b424
Ahn YoungSeon <dudtjs0920@naver.com>
2021-08-04 13:27:39 +0900

    VSTR-68 SEED 암호화 base64 encoding 추가

--- src/main/java/com/ftseoul/visitor/encrypt/Seed.java
+++ src/main/java/com/ftseoul/visitor/encrypt/Seed.java
@@ -1,27 +1,38 @@
 package com.ftseoul.visitor.encrypt;
 
 import com.ftseoul.visitor.config.EncryptConfig;
+import org.springframework.util.SerializationUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class Seed {
 
-    private final EncryptConfig encrypt = new EncryptConfig();
+    private static final EncryptConfig encrypt = new EncryptConfig();
 
-    private final String key = encrypt.getKey();
-    private final String IV = encrypt.getIV();
+    private static final String key = encrypt.getKey();
+    private static final String IV = encrypt.getIV();
 
-    private final byte[] pbszUserKey = key.getBytes(StandardCharsets.UTF_8);
-    private final byte[] pbszIV = IV.getBytes(StandardCharsets.UTF_8);
+    private static final byte[] pbszUserKey = key.getBytes(StandardCharsets.UTF_8);
+    private static final byte[] pbszIV = IV.getBytes(StandardCharsets.UTF_8);
 
-    public byte[] encrypt(String rawMsg) {
+    public static String encrypt(String rawMsg) {
         byte[] message = rawMsg.getBytes(StandardCharsets.UTF_8);
         byte[] encryptedMsg = KISA_SEED_CBC.SEED_CBC_Encrypt(pbszUserKey, pbszIV,
                 message, 0, message.length);
-        return encryptedMsg;
+
+        return Base64.getEncoder()
+                .encodeToString(SerializationUtils.serialize(encryptedMsg));
     }
 
-    public String decrypt(byte[] encryptedMsg) {
+    private static <T> T deserialize(String code, Class<T> cls) {
+        return cls.cast(SerializationUtils.deserialize(
+                Base64.getDecoder().decode(code)
+        ));
+    }
+
+    public static String decrypt(String encodeMsg) {
+        byte[] encryptedMsg = deserialize(encodeMsg, byte[].class);
         byte[] decryptedMsg = KISA_SEED_CBC.SEED_CBC_Decrypt(pbszUserKey, pbszIV,
                 encryptedMsg, 0, encryptedMsg.length);
         return new String(decryptedMsg, StandardCharsets.UTF_8);
