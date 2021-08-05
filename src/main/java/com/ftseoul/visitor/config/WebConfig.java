8a1cfb8b7c6c3d0368d4f0b407a72e9ccee235a4
Ahn YoungSeon <dudtjs0920@naver.com>
2021-08-05 23:20:08 +0900

    VSTR0-68 CORS origin add api.visitor.dev.42seoul.io

--- src/main/java/com/ftseoul/visitor/config/WebConfig.java
+++ src/main/java/com/ftseoul/visitor/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "OPTIONS", "POST", "HEAD", "DELETE", "PUT", "PATCH")
                 .maxAge(3600)
                 .allowCredentials(true)
-                .allowedOrigins("http://localhost:3000", "https://visitor.dev.42seoul.io", "http://visitor.dev.42seoul.io");
+                .allowedOrigins("http://localhost:3000", "https://visitor.dev.42seoul.io", "http://visitor.dev.42seoul.io", "https://api.visitor.dev.42seoul.io");
     }
 
     @Override
