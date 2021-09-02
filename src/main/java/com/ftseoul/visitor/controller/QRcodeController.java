5c55bcc779b3c4fd04dc5a14e8a6cb7b80201708
최재형 <dang0764@naver.com>
2021-09-02 12:49:58 +0900

    VSTR-67 QRcheck API 수정 - JSON을 받는 방식에서 form-urlencoded 방식으로 처리하도록 수정

--- src/main/java/com/ftseoul/visitor/controller/QRcodeController.java
+++ src/main/java/com/ftseoul/visitor/controller/QRcodeController.java
@@ -1,17 +1,12 @@
 package com.ftseoul.visitor.controller;
 
-import com.ftseoul.visitor.data.visitor.VisitorStatus;
 import com.ftseoul.visitor.dto.QRCheckResponseDto;
-import com.ftseoul.visitor.dto.payload.QRCodePayload;
-import com.ftseoul.visitor.encrypt.Seed;
-import com.ftseoul.visitor.exception.ResourceNotFoundException;
 import com.ftseoul.visitor.service.QRcodeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,10 +17,10 @@ public class QRcodeController {
     private final QRcodeService qRcodeService;
 
     @PostMapping("/qrcode")
-    public ResponseEntity<?> qrcodeCheck(@RequestBody QRCodePayload qrCodePayload) {
-        log.info("Check qrcode text: {}", qrCodePayload.getCode());
-        String code = qRcodeService.decodeQRText(qrCodePayload.getCode());
-        QRCheckResponseDto qrCheckResponseDto = qRcodeService.checkQRCode(code);
+    public ResponseEntity<?> qrcodeCheck(String code) {
+        log.info("Check qrcode text: {}", code);
+        String decodeQRText = qRcodeService.decodeQRText(code);
+        QRCheckResponseDto qrCheckResponseDto = qRcodeService.checkQRCode(decodeQRText);
         return new ResponseEntity<>(qrCheckResponseDto, HttpStatus.OK);
     }
 
