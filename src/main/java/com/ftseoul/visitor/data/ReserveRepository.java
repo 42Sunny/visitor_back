786c4d567e07260f9de8964bf6b7b8b90f0c012f
최재형 <dang0764@naver.com>
2021-10-11 15:16:48 +0900

    VSTR-134 인포 서비스 테스트 추가 - 임시로 추가한 jacoco 제거 - Native 쿼리에 Param annotation 추가(테스트 환경시에만 쿼리인지가 안되던 문제)

--- src/main/java/com/ftseoul/visitor/data/ReserveRepository.java
+++ src/main/java/com/ftseoul/visitor/data/ReserveRepository.java
@@ -6,16 +6,15 @@ import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReserveRepository extends JpaRepository<Reserve, Long> {
 
     @Query("SELECT new com.ftseoul.visitor.dto.reserve.DateFoundResponseDto(r.id , r.place, s.id, s.name, s.phone, r.purpose , r.date) "
         + "FROM Reserve r INNER JOIN FETCH Staff s ON r.targetStaff = s.id "
         + "WHERE r.date BETWEEN :from AND :to")
-    List<DateFoundResponseDto> findAllReserveWithStaffByDate(LocalDateTime from, LocalDateTime to);
+    List<DateFoundResponseDto> findAllReserveWithStaffByDate(@Param(value = "from") LocalDateTime from,
+        @Param(value = "to") LocalDateTime to);
 
     List<Reserve> findAllByTargetStaff(Long targetStaff);
-
-    @Transactional
-    void deleteAllById(long id);
 }
---
+++ src/test/java/com/ftseoul/visitor/service/InfoServiceTest.java
@@ -0,0 +1,111 @@
+package com.ftseoul.visitor.service;
+
+import com.ftseoul.visitor.data.Reserve;
+import com.ftseoul.visitor.data.ReserveRepository;
+import com.ftseoul.visitor.data.Staff;
+import com.ftseoul.visitor.data.StaffRepository;
+import com.ftseoul.visitor.data.Visitor;
+import com.ftseoul.visitor.data.VisitorRepository;
+import com.ftseoul.visitor.data.visitor.VisitorStatus;
+import com.ftseoul.visitor.dto.payload.VisitorStatusInfo;
+import com.ftseoul.visitor.dto.reserve.DateFoundResponseDto;
+import com.ftseoul.visitor.dto.visitor.UpdateVisitorStatusDto;
+import com.ftseoul.visitor.encrypt.Seed;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional
+@Rollback(value = true)
+class InfoServiceTest {
+    @Autowired
+    private InfoService infoService;
+
+    @Autowired
+    private Seed seed;
+
+    @Autowired
+    private ReserveRepository reserveRepository;
+
+    @Autowired
+    private StaffRepository staffRepository;
+
+    @Autowired
+    private VisitorRepository visitorRepository;
+
+    Staff savedStaff;
+
+    Reserve savedReserve;
+
+    Visitor savedVisitor;
+
+    @BeforeAll
+    void setup() {
+        init();
+    }
+
+    @AfterAll
+    void cleanup() {
+        finish();
+    }
+
+    void init() {
+        Staff staff = Staff
+            .builder()
+            .name(seed.encrypt("abcde"))
+            .phone(seed.encrypt("01012345678"))
+            .build();
+        savedStaff = staffRepository.save(staff);
+
+        Reserve reserve = Reserve
+            .builder()
+            .targetStaff(savedStaff.getId())
+            .purpose("방문")
+            .date(LocalDateTime.now())
+            .place("개포")
+            .build();
+        savedReserve = reserveRepository.save(reserve);
+
+        Visitor visitor = Visitor
+            .builder()
+            .name(seed.encrypt("홍길동님"))
+            .organization("이노베이션아카데미")
+            .reserve_id(savedReserve.getId())
+            .phone(seed.encrypt("01011112222"))
+            .build();
+        savedVisitor = visitorRepository.save(visitor);
+    }
+
+    void finish() {
+        reserveRepository.delete(savedReserve);
+        visitorRepository.delete(savedVisitor);
+        staffRepository.delete(savedStaff);
+    }
+
+    @Test
+    void 날짜별조회() {
+        List<DateFoundResponseDto> list = infoService.findAllByDate(
+            LocalDateTime.now().toLocalDate());
+        assertNotEquals(list.size(), 0);
+    }
+
+    @Test
+    void 방문자상태변경() {
+        UpdateVisitorStatusDto dto = new UpdateVisitorStatusDto
+            (new VisitorStatusInfo(savedVisitor.getId(),
+            VisitorStatus.퇴실));
+        infoService.changeVisitorStatus(dto);
+        assertEquals(VisitorStatus.퇴실, visitorRepository.findById(savedVisitor.getId()).get().getStatus());
+    }
+}
