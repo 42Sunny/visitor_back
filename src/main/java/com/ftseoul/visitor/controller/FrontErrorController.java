80894eb382c7bda30d99a0d45d2a2666baa7ad14
최재형 <dang0764@naver.com>
2021-10-19 20:35:44 +0900

    VSTR-147 상용서버 URL 분리

--- src/main/java/com/ftseoul/visitor/controller/FrontErrorController.java
+++ src/main/java/com/ftseoul/visitor/controller/FrontErrorController.java
@@ -3,6 +3,7 @@ package com.ftseoul.visitor.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ftseoul.visitor.dto.error.FrontErrorResponseDto;
 import java.util.Map;
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/ftseoul/visitor/util/Constants.java
+++ src/main/java/com/ftseoul/visitor/util/Constants.java
@@ -1,7 +1,8 @@
 package com.ftseoul.visitor.util;
 
 public abstract class Constants {
-    public static final String DOMAIN = "https://dev.vstr.kr";
-    public static final String QRPATH = "https://visitor.dev.42seoul.io/qr/";
-    public static final String LOOKUPPATH = "https://visitor.dev.42seoul.io/reserve-info/";
+    public static final String PROFILE = "spring.profiles.active";
+    public static final String DOMAIN = (System.getProperty(PROFILE).equals("prod")) ? "https://vstr.kr" : "https://dev.vstr.kr";
+    public static final String QRPATH = (System.getProperty(PROFILE).equals("prod")) ? "https://visitor.42seoul.io/qr/" : "https://visitor.dev.42seoul.io/qr/";
+    public static final String LOOKUPPATH = (System.getProperty(PROFILE).equals("prod")) ? "https://visitor.42seoul.io/reserve-info/": "https://visitor.dev.42seoul.io/reserve-info/";
 }
