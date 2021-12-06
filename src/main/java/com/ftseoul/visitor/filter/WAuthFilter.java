93da6d2d3173fd1dd2f0b41b102bf783ea348eeb
박건욱 <saiver500@naver.com>
2021-12-06 08:58:47 +0000

    Merged in feature/add_authorization (pull request #11)
    w_auth 인증 추가
    
    * VSTR-193 환경변수 설정
    
    * VSTR-193 w_auth 쿠키 확인 및 정상작동 테스트
    
    * VSTR-193 mysql 환경변수명 변경
    
    * Merge branch 'develop' of bitbucket.org:42seoul/visitor_server into feature/add_authorization
    
    * VSTR-193 cookie 변환 코드 추가 및 쿠키가 존재하지 않을 경우 추가
    
    * VSTR-193 RestTemplate 에러로 인한 쿠키 값 및 X-Authkey 확인
    
    * VSTR-193 쿠키 변환 코드 변경.
    
    * VSTR-193 권한 필요 함수 일괄 적용 및 불필요 코드 삭제
    
    
    Approved-by: 황지훈

---
+++ src/main/java/com/ftseoul/visitor/filter/WAuthFilter.java
@@ -0,0 +1,61 @@
+package com.ftseoul.visitor.filter;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+@Slf4j
+public class WAuthFilter {
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${wauth.cookiename}")
+    private String cookieName;
+    @Value("${wauth.X_AUTH_KEY}")
+    private String X_AUTH_KEY;
+    @Value("${wauth.statusurl}")
+    private String statusUrl;
+
+    public boolean isAuthorized(HttpServletRequest httpServletRequest) {
+        if (httpServletRequest.getCookies() == null) {
+            log.error("쿠키가 존재하지 않습니다.");
+            return false;
+        }
+        Optional<Cookie> w_auth = Arrays.stream(httpServletRequest.getCookies()).
+                filter(cookie -> cookie.getName().equals(cookieName)).findFirst();
+        if (w_auth.isPresent()) {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add(HttpHeaders.COOKIE, CookieConverter(w_auth.get()));
+            log.info(cookieName + " : " + CookieConverter(w_auth.get()));
+            httpHeaders.set("x-42cadet-auth-key", X_AUTH_KEY);
+            HttpEntity<String> httpEntity = new HttpEntity<>(null, httpHeaders);
+            ResponseEntity<JsonNode> exchange = restTemplate.exchange(statusUrl, HttpMethod.GET, httpEntity, JsonNode.class
+            );
+            if (exchange.getBody() == null)
+                return false;
+            return exchange.getBody().get("isAdmin").asBoolean();
+        } else {
+            log.error("어드민 계정으로 다시 시도해주세요.");
+            return false;
+        }
+    }
+
+    private String CookieConverter(Cookie cookie) {
+        return ResponseCookie.from(cookie.getName(), cookie.getValue())
+                .domain(cookie.getDomain())
+                .path(cookie.getPath())
+                .maxAge(cookie.getMaxAge())
+                .build()
+                .toString();
+    }
+}
