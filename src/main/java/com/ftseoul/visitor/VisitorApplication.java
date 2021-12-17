71c9b8107561a2268a8efafe65fe7c8f09bf8937
ghkdwlgns612 <ghkdwlgns612@github.com>
2021-12-17 17:12:39 +0900

    VSTR-213 @RefreshScope 클래스 단위설정.

--- src/main/java/com/ftseoul/visitor/VisitorApplication.java
+++ src/main/java/com/ftseoul/visitor/VisitorApplication.java
@@ -2,11 +2,9 @@ package com.ftseoul.visitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@RefreshScope
 @SpringBootApplication
 public class VisitorApplication {
 
--- src/main/java/com/ftseoul/visitor/config/EncryptConfig.java
+++ src/main/java/com/ftseoul/visitor/config/EncryptConfig.java
@@ -2,10 +2,12 @@ package com.ftseoul.visitor.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Configuration
+@RefreshScope
 public class EncryptConfig {
 
     @Value("${encrypt.seed}")
