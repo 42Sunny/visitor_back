d0ce78a7e5a74ec15aa4f509e629e01f4c2bbc74
ghkdwlgns612 <ghkdwlgns612@github.com>
2021-12-21 15:54:28 +0900

    VSTR-216 200번대 응답으로 교체.

--- src/main/java/com/ftseoul/visitor/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ftseoul/visitor/exception/GlobalExceptionHandler.java
@@ -27,21 +27,21 @@ public class GlobalExceptionHandler {
         webSocketService.sendMessageToSubscriber("/visitor", e.getMessage());
         log.warn("예약 날짜가 오늘이 아닙니다. 예약 날짜 : {}", e.getDate());
         ErrorResponse response = ErrorResponse.of(ErrorCode.UNAUTHORIZED_CODE,new ArrayList<>());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @ExceptionHandler(InvalidQRCodeException.class)
     protected ResponseEntity<ErrorResponse> invalidQRCodeExceptionHandler(InvalidQRCodeException e) {
         log.warn("유효하지 않은 QR코드 입니다. QR값 : {}", e.getCode());
         ErrorResponse response = ErrorResponse.of(ErrorCode.NOT_FOUND_QRCODE,new ArrayList<>());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @ExceptionHandler(PhoneDuplicatedException.class)
     protected ResponseEntity<ErrorResponse> phoneDuplicatedExceptionHandler(PhoneDuplicatedException e) {
         log.warn("중복되는 휴대전화 번호입니다. 다시 입력해주세요.");
         ErrorResponse response = ErrorResponse.of(ErrorCode.DUPLICATE_PHONE_NUMBER,new ArrayList<>());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -49,7 +49,7 @@ public class GlobalExceptionHandler {
         log.warn("Validation 오류입니다. 확인바랍니다.", e.getMessage());
         final List<FieldError> fieldErrors = FieldError.of(e.getBindingResult());
         ErrorResponse response = ErrorResponse.of(ErrorCode.UNAUTHORIZED_CODE,fieldErrors);
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -57,27 +57,27 @@ public class GlobalExceptionHandler {
         log.warn("해당 필드 {} 의 값 {}을 찿지 못했습니다.",e.getFieldName(),e.getFieldValue());
         final List<FieldError> fieldErrors = FieldError.bindResourceNotFoundException(e);
         ErrorResponse response = ErrorResponse.of(ErrorCode.RESOURCE_NOT_FOUND,fieldErrors);
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @ExceptionHandler(WAuthUnAuthorizedException.class)
     protected ResponseEntity<ErrorResponse> wAuthUnAuthorizedException(WAuthUnAuthorizedException e) {
         log.warn("w_auth인증에 실패하였습니다.");
         ErrorResponse response = ErrorResponse.of(ErrorCode.WAUTH_AUTHORIZED_FAIL,new ArrayList<>());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @ExceptionHandler(InvalidDeviceException.class)
     protected ResponseEntity<ErrorResponse> invalidDeviceExceptionHandler(InvalidDeviceException e) {
         log.error("허용되지 않은 기기 : {}",e.getDeviceId());
         ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_DEVICE,new ArrayList<>());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> exceptionHandler(Exception e) {
         log.error("서버내부 오류입니다. 에러의 내용은 다음과 같습니다. 에러 내용 : {}",e.getMessage());
         ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, new ArrayList<>());
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
