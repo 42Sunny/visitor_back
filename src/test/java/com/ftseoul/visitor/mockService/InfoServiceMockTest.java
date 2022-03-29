58de9b3d7bd0ad18c9b6bb6493f197984f24c746
SIOUkoeran <alstnalsgud@gmail.com>
2022-03-29 21:23:25 +0900

    VSTR-134 Test InfoMockTest 완료

--- src/test/java/com/ftseoul/visitor/mockService/InfoServiceMockTest.java
+++ src/test/java/com/ftseoul/visitor/mockService/InfoServiceMockTest.java
@@ -1,27 +1,58 @@
 package com.ftseoul.visitor.mockService;
 
+import com.ftseoul.visitor.data.Visitor;
+import com.ftseoul.visitor.data.visitor.VisitorStatus;
+import com.ftseoul.visitor.dto.payload.VisitorStatusInfo;
+import com.ftseoul.visitor.dto.reserve.DateFoundResponseDto;
+import com.ftseoul.visitor.dto.visitor.UpdateVisitorStatusDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import com.ftseoul.visitor.data.QueryRepository;
-import com.ftseoul.visitor.data.ReserveRepository;
-import com.ftseoul.visitor.data.Staff;
-import com.ftseoul.visitor.data.VisitorRepository;
-import com.ftseoul.visitor.encrypt.Seed;
-import com.ftseoul.visitor.service.InfoService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
-import java.lang.ref.Reference;
-import java.nio.charset.StandardCharsets;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class InfoServiceMockTest extends MockInfoBaseTest{
 
+    @Test
+    @DisplayName("날짜별_조회_빈리스트_반환")
+    void searchByDateReturnEmptyList(){
+
+        //given
+        //Nothing
+
+        //when
+        doReturn(List.of()).when(reserveRepository).findAllReserveWithStaffByDate(any(), any());
+
+        //then
+        final List<DateFoundResponseDto> findList = infoService.findAllByDate(LocalDateTime.now().toLocalDate());
+        Assertions.assertThat(findList.size()).isEqualTo(0);
+    }
 
 
+    @Test
+    @DisplayName("방문자_상태_변경")
+    void changeVisitorStatus(){
 
+        //given
+        UpdateVisitorStatusDto dto = new UpdateVisitorStatusDto(new VisitorStatusInfo(visitor.getId(), VisitorStatus.퇴실));
+        Visitor checkoutVisitor = visitor;
+        checkoutVisitor.checkOut();
+
+        //when
+        doReturn(Optional.of(visitor)).when(visitorRepository).findById(1L);
+        doReturn(checkoutVisitor).when(visitorRepository).save(any());
+        final VisitorStatusInfo visitorStatusInfo = infoService.changeVisitorStatus(dto);
+
+        //then
+        Assertions.assertThat(visitorStatusInfo.getStatus()).isEqualTo(VisitorStatus.퇴실);
+
+        //verify
+        verify(visitorRepository, times(1)).findById(anyLong());
+        verify(visitorRepository, times(1)).save(any());
+    }
 }
--- src/test/java/com/ftseoul/visitor/mockService/MockInfoBaseTest.java
+++ src/test/java/com/ftseoul/visitor/mockService/MockInfoBaseTest.java
@@ -1,12 +1,10 @@
 package com.ftseoul.visitor.mockService;
 
 
-import com.ftseoul.visitor.data.QueryRepository;
-import com.ftseoul.visitor.data.ReserveRepository;
-import com.ftseoul.visitor.data.Staff;
-import com.ftseoul.visitor.data.VisitorRepository;
+import com.ftseoul.visitor.data.*;
 import com.ftseoul.visitor.encrypt.Seed;
 import com.ftseoul.visitor.service.InfoService;
+import org.aspectj.util.Reflection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,21 +14,26 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 @ExtendWith(MockitoExtension.class)
 public class MockInfoBaseTest {
     @InjectMocks
-    private InfoService infoService;
+    protected InfoService infoService;
 
     @Mock
-    private ReserveRepository reserveRepository;
+    protected ReserveRepository reserveRepository;
 
     @Mock
-    private VisitorRepository visitorRepository;
+    protected VisitorRepository visitorRepository;
 
     @Mock
-    private QueryRepository queryRepository;
+    protected QueryRepository queryRepository;
 
+
+    protected Staff staff;
+    protected Reserve reserve;
+    protected Visitor visitor;
     @Spy
     Seed seed;
 
@@ -41,13 +44,31 @@ public class MockInfoBaseTest {
         ReflectionTestUtils.setField(seed, "pbszUserKey", "visitor987654321".getBytes(StandardCharsets.UTF_8));
         ReflectionTestUtils.setField(seed, "pbszIV", "visitor987654321".getBytes(StandardCharsets.UTF_8));
 
-        Staff staff = Staff
+        staff = Staff
                 .builder()
                 .name(seed.encrypt("abcde"))
                 .phone(seed.encrypt("01012345678"))
                 .department("시설관리")
                 .build();
+        ReflectionTestUtils.setField(staff, "id",1L);
 
+        reserve = Reserve
+                .builder()
+                .targetStaff(1L)
+                .purpose("방문")
+                .date(LocalDateTime.now())
+                .place("개포")
+                .build();
+        ReflectionTestUtils.setField(reserve, "id",1L);
+
+        visitor = Visitor.builder()
+                .name(seed.encrypt("지울이름"))
+                .reserve_id(1L)
+                .phone(seed.encrypt("01055555555"))
+                .organization("이노베이션아카데미")
+                .build();
+        ReflectionTestUtils.setField(visitor, "id", 1L);
     }
+
     
 }
